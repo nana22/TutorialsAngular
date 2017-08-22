b0cd3ca3f70648c5e91502cb34ff44d44ab575e1
darce <nana22059@gmail.com>
2017-08-22 15:08:20 -0600

    first component

---
+++ Tutorials/Angular/First/my-app/src/app/components/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template:`
+       <h1> este es el header</h1>
+  `
+})
+
+export class HeaderComponent {
+
+}
